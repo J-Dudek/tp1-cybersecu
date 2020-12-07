abcb38b84bb967b2bc4ccc283d76aeed31e83271
Julien Dudek <julien.dudek@gmail.com>
2020-12-07 15:42:30 +0100

    :adhesive_bandage: add annotations

--- src/main/java/tp/securite/tp1/repositories/BookRepository.java
+++ src/main/java/tp/securite/tp1/repositories/BookRepository.java
@@ -1,5 +1,9 @@
 package tp.securite.tp1.repositories;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tp.securite.tp1.model.Book;
 
-public interface BookRepository {
+@Repository
+public interface BookRepository extends CrudRepository<Book,Long> {
 }
