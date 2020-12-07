4a6d5ba5c3aeefbb237fe2ad35dcfb95c61ef22a
Julien Dudek <julien.dudek@gmail.com>
2020-12-07 16:18:12 +0100

    🗃 add constraints

--- src/main/java/tp/securite/tp1/model/Book.java
+++ src/main/java/tp/securite/tp1/model/Book.java
@@ -1,11 +1,14 @@
 package tp.securite.tp1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -18,4 +21,9 @@ public class Book {
     private String auteur;
     private String titre;
     private int nbpages;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
 }
